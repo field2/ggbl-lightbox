439a895c88951cc01bb88f0b8aa5b4f1fde2c5a7
Ben Dunkle <ben@field2.com>
2025-05-22 09:53:35 -0400

    added gitignore

---
+++ tags/1.2.2/ggbl-lightbox.js
@@ -0,0 +1,128 @@
+jQuery(document).ready(function($) {
+	// global variables for script
+	var current, size;
+	$('.wp-block-gallery a').click(function(e) {
+		// prevent default click event
+		e.preventDefault();
+		// get href from clicked element
+		var image_href = $(this).attr("href");
+		console.log(image_href);
+		// determine the index of clicked trigger
+		var slideNum = $('.wp-block-gallery a').index(this);
+		console.log(slideNum);
+
+		// find out if #ggbl_lightbox exists
+		if ($('#ggbl_lightbox').length > 0) {
+			// if it does
+			$('#ggbl_lightbox').fadeIn(300);
+			// otherwise
+		} else {
+			// create HTML markup for lightbox window
+			var lightbox =
+				'<div id="ggbl_lightbox">' +
+				'<div class="ggbl_close"></div>' +
+				'<ul id="ggbl_slider"></ul>' +
+				'<div class="ggbl_nav">' +
+				'<a href="#" class="ggbl_prev ggbl_slide-nav"></a>' +
+				'<a href="#" class="ggbl_next ggbl_slide-nav"></a>' +
+				'</div><!-- .ggbl_nav -->' +
+				'</div><!-- #ggbl_lightbox -->';
+			// add lightbox HTML to the DOM
+			$('body').append(lightbox);
+			// make sure the user can't scroll while the lightbox is showing
+			$('body').addClass("ggbl_noscroll");
+			// fill lightbox with .blocks-gallery-grid a hrefs
+			$('.wp-block-gallery').find('.wp-block-image a').each(function() {
+				var $href = $(this).attr('href');
+				$('#ggbl_slider').append(
+					'<li>' +
+					'<img src="' + $href + '">' +
+					'</li>'
+				);
+			});
+		}
+		// set the slider size based on number of objects in slideshow
+		size = $('#ggbl_slider > li').length;
+		// hide all slides, then show the selected slide
+		$('#ggbl_slider > li').hide();
+		$('#ggbl_slider > li:eq(' + slideNum + ')').show();
+		// set current to selected slide
+		current = slideNum;
+	});
+	// click anywhere on the page to get rid of lightbox window
+	$('body').on('click', '#ggbl_lightbox', function() { // using .on() instead of .live(). more modern, and fixes event bubbling issues
+		$('#ggbl_lightbox').fadeOut(300);
+		$('body').removeClass("ggbl_noscroll");
+	});
+	// navigation left/right arrow keys, esc to close
+
+	document.onkeydown = checkKey;
+
+	function checkKey(e) {
+
+		e = e || window.event;
+		if (e.keyCode == '37') {
+			e.preventDefault();
+			e.stopPropagation();
+			var $this = $(this);
+			var dest;
+			dest = current - 1;
+			if (dest < 0) {
+				dest = size - 1;
+			}
+			// fadeOut current slide, fadeIn next/prev slide
+			$('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
+			$('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
+			// update current slide
+			current = dest;
+		} else if (e.keyCode == '39') {
+			e.preventDefault();
+			e.stopPropagation();
+			var $this = $(this);
+			var dest;
+			dest = current + 1;
+			if (dest > size - 1) {
+				dest = 0;
+			}
+
+			// fadeOut current slide, fadeIn next/prev slide
+			$('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
+			$('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
+			// update current slide
+			current = dest;
+		} else if (e.keyCode == '27') {
+			$('#ggbl_lightbox').fadeOut(300);
+			$('body').removeClass("ggbl_noscroll");
+		}
+
+	}
+
+
+
+	// navigation prev/next
+	$('body').on('click', '.ggbl_slide-nav', function(e) {
+		// prevent link from firing and lightbox from accidentally closing
+		e.preventDefault();
+		e.stopPropagation();
+		var $this = $(this);
+		var dest;
+		// check if prev arrow was clicked
+		if ($this.hasClass('ggbl_prev')) {
+			dest = current - 1;
+			if (dest < 0) {
+				dest = size - 1;
+			}
+		} else {
+			// otherwise they clicked next
+			dest = current + 1;
+			if (dest > size - 1) {
+				dest = 0;
+			}
+		}
+		// fadeOut current slide, fadeIn next/prev slide
+		$('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
+		$('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
+		// update current slide
+		current = dest;
+	});
+});
---
+++ trunk/readme.txt
@@ -0,0 +1,46 @@
+=== Gutenberg Gallery Blocks Lightbox  ===
+Contributors: empireoflight
+Tags: Block, gallery, lightbox
+Requires at least: 5.0
+Tested up to: 6.6.2
+Stable tag: 1.2.4
+License: GPL-2.0
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+This plugin transforms the Gutenberg gallery block into a lightbox with previous/next navigation. It was built to offer those who wish to present a series of images in lightbox/slider format with a simple, lightweight solution.
+
+= Installation =
+
+* Install the plugin through the Install Plugins interface or by uploading the `ggbl-lightbox` folder to your `/wp-content/plugins/` directory.
+* Activate the plugin.
+* Make sure you select "Media File" in the Link To dropdown menu for your galleries
+
+
+= Why did you make this plugin? =
+
+I haven't found a lightbox that is optimized for Gutenberg galleries. Many generate their own gallery formats or contain unneccesary options. I wanted to build one that was as minimal as possible, while offering a functional and elegant experience.
+ 
+ == Changelog ==
+
+=1.2.4 =
+* Tested for wp 6.6.2. Removed tags, updated plugin uri
+
+=1.2.3 =
+* Tested for wp 6.5
+
+=1.2.2 =
+* Changed lightbox to position:fixed to prevent unwanted scrolling. Props @Antahai
+
+=1.2.1 =
+* test for wp 6.0. Adds support for left/right arrow key navigation. Adds support for ESC key to close gallery.
+
+=1.2 =
+* test for wp 5.9. Works with updated wp-blocks-gallery class
+
+=1.1 =
+* Ensure that lightbox is above all other elements on the page
+
+=1.0 =
+* Initial Release.
