aa41baeca48fd06a3b360e0b471d3c2f1c2c6c6a
Ben Dunkle <ben@field2.com>
2025-05-22 10:27:26 -0400

    added swipe to mobile

--- trunk/ggbl-lightbox.js
+++ trunk/ggbl-lightbox.js
@@ -54,79 +54,71 @@ jQuery(document).ready(function($) {
 		$('#ggbl_lightbox').fadeOut(300);
 		$('body').removeClass("ggbl_noscroll");
 	});
-	// navigation left/right arrow keys, esc to close
-
-	document.onkeydown = checkKey;
-
-	function checkKey(e) {
-
-		e = e || window.event;
-		if (e.keyCode == '37') {
-			e.preventDefault();
-			e.stopPropagation();
-			var $this = $(this);
-			var dest;
-			dest = current - 1;
-			if (dest < 0) {
-				dest = size - 1;
-			}
-			// fadeOut current slide, fadeIn next/prev slide
-			$('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
-			$('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
-			// update current slide
-			current = dest;
-		} else if (e.keyCode == '39') {
-			e.preventDefault();
-			e.stopPropagation();
-			var $this = $(this);
-			var dest;
-			dest = current + 1;
-			if (dest > size - 1) {
-				dest = 0;
-			}
-
-			// fadeOut current slide, fadeIn next/prev slide
-			$('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
-			$('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
-			// update current slide
-			current = dest;
-		} else if (e.keyCode == '27') {
-			$('#ggbl_lightbox').fadeOut(300);
-			$('body').removeClass("ggbl_noscroll");
-		}
-
-	}
-
-
+	var swipeInProgress = false;
 
 	// navigation prev/next
 	$('body').on('click', '.ggbl_slide-nav', function(e) {
-		// prevent link from firing and lightbox from accidentally closing
-		e.preventDefault();
-		e.stopPropagation();
-		var $this = $(this);
-		var dest;
-		// check if prev arrow was clicked
-		if ($this.hasClass('ggbl_prev')) {
-			dest = current - 1;
-			if (dest < 0) {
-				dest = size - 1;
-			}
-		} else {
-			// otherwise they clicked next
-			dest = current + 1;
-			if (dest > size - 1) {
-				dest = 0;
-			}
-		}
-		// fadeOut current slide, fadeIn next/prev slide
-		$('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
-		$('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
-		// update current slide
-		current = dest;
+	    e.preventDefault();
+	    e.stopPropagation();
+	    var $this = $(this);
+	    var dest;
+	    if ($this.hasClass('ggbl_prev')) {
+	        dest = current - 1;
+	        if (dest < 0) dest = size - 1;
+	    } else {
+	        dest = current + 1;
+	        if (dest > size - 1) dest = 0;
+	    }
+	    if (!swipeInProgress) {
+	        $('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
+	        $('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
+	    } else {
+	        swipeInProgress = false; // Move this line BEFORE .show()
+	        $('#ggbl_slider > li:eq(' + current + ')').hide();
+	        $('#ggbl_slider > li:eq(' + dest + ')').show();
+	    }
+	    current = dest;
 	});
 
-	// Swipe navigation for mobile
+	// navigation left/right arrow keys, esc to close
+	document.onkeydown = function(e) {
+	    e = e || window.event;
+	    var dest;
+	    if (e.keyCode == '37') {
+	        e.preventDefault();
+	        e.stopPropagation();
+	        dest = current - 1;
+	        if (dest < 0) dest = size - 1;
+	        if (!swipeInProgress) {
+	            $('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
+	            $('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
+	        } else {
+	            swipeInProgress = false; // Move this line BEFORE .show()
+	            $('#ggbl_slider > li:eq(' + current + ')').hide();
+	            $('#ggbl_slider > li:eq(' + dest + ')').show();
+	        }
+	        current = dest;
+	    } else if (e.keyCode == '39') {
+	        e.preventDefault();
+	        e.stopPropagation();
+	        dest = current + 1;
+	        if (dest > size - 1) dest = 0;
+	        if (!swipeInProgress) {
+	            $('#ggbl_slider > li:eq(' + current + ')').fadeOut(750);
+	            $('#ggbl_slider > li:eq(' + dest + ')').fadeIn(750);
+	        } else {
+	            swipeInProgress = false; // Move this line BEFORE .show()
+	            $('#ggbl_slider > li:eq(' + current + ')').hide();
+	            $('#ggbl_slider > li:eq(' + dest + ')').show();
+	        }
+	        current = dest;
+	    } else if (e.keyCode == '27') {
+	        $('#ggbl_lightbox').fadeOut(300);
+	        $('body').removeClass("ggbl_noscroll");
+	    }
+	};
+
+	// Swipe navigation for mobile with slide animation
 	var touchStartX = null;
 	var touchEndX = null;
 	var swipeThreshold = 50; // Minimum px distance for swipe
@@ -147,13 +139,47 @@ jQuery(document).ready(function($) {
 	    if (touchStartX !== null && touchEndX !== null) {
 	        var diff = touchStartX - touchEndX;
 	        if (Math.abs(diff) > swipeThreshold) {
+	            var $slides = $('#ggbl_slider > li');
+	            var $currentSlide = $slides.eq(current);
+	            var dest, direction;
 	            if (diff > 0) {
-	                // Swipe left: next
-	                $('.ggbl_next').trigger('click');
+	                dest = current + 1;
+	                if (dest > size - 1) dest = 0;
+	                direction = 'left';
 	            } else {
-	                // Swipe right: prev
-	                $('.ggbl_prev').trigger('click');
+	                dest = current - 1;
+	                if (dest < 0) dest = size - 1;
+	                direction = 'right';
 	            }
+	            var $nextSlide = $slides.eq(dest);
+
+	            // Set up parent for sliding
+	            $('#ggbl_slider').css({position: 'relative', overflow: 'hidden'});
+
+	            // Prepare next slide position
+	            $nextSlide.css({
+	                display: 'block',
+	                position: 'absolute',
+	                left: (direction === 'left' ? '100%' : '-100%'),
+	                top: 0,
+	                width: '100%'
+	            });
+
+	            // Animate slides
+	            $currentSlide
+	                .css({position: 'absolute', left: 0, top: 0, width: '100%'})
+	                .animate({left: (direction === 'left' ? '-100%' : '100%')}, 400, function() {
+	                    $(this).hide().css({position: '', left: '', top: '', width: ''});
+	                });
+	            $nextSlide
+	                .animate({left: '0%'}, 400, function() {
+	                    $(this).css({position: '', left: '', top: '', width: ''});
+	                    // Hide all other slides just in case
+	                    $slides.not($(this)).hide();
+	                });
+
+	            current = dest;
+	            swipeInProgress = true;
 	        }
 	    }
 	    // Reset
